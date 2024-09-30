0adf5336d12c3d8fff2ce970060e24c63b6adb93
Shravan <122885892+shanisog@users.noreply.github.com>
2024-09-30 11:48:22 +0530

    Update nextdate.c

--- nextdate.c
+++ nextdate.c
@@ -1,99 +1,44 @@
-#include<stdio.h>
-int check(int day,int month){
-if((month==4||month==6||month==9||month==11)&&day==31)
-return 1;
-else
-return 0;
-}
-int isleep(int year){
-if((year%4==0)&&(year%100=0)&&(year%00==0))
-return 1;
-else
-return 0;
-}
-int main(){
-int day,month,year,tmonth,tyear,tday;
-char flag;
-do{
-flag='y';
-printf("enter todays date in ddmmyy form\n");
-scanf("%d%d%d',&day,&month,&year);
-tmonth=month;
-tyear=year;
-if(day<1||day>31){
-printf("value of day not in rannge 1--31\n");
-flag='n';
-}
-if(month<1||month>12){
-printf("value of month not in rannge 1--12\n");
-flag='n';
-}
-else  if(check(day,month))
-{
-printf("value of day not in range <=30");
-flag='n';
-}
-if(year<1812 || year >2018)
-{
-printf("value of year not in range 18-12-2018");
-flag='n';
-}if (month==2)
-{if (isleap(year)&&day>day>29)
-{printf("invalid input for leap year");
-flag='n';
-}}}while(flag=='n');
-switch(month){
-case1:
-case3:
-case5:
-case7:
-case8:
-case10:if (day<31)
-tday=day+1;
-else
-{
-tday=1;
-tmonth=month+1;
-}
-break;
-case4:
-case6:
-case9:
-case11:if(day<30)
-tday=day+1;
-else{
-tday=1;
-else{
-tday=1;
-tmonth=month+1;
-}
-break;
-case12:if(day<31)
-tday=day+!;
-else{
-tday=day+1;
-else{
-tday=1;
-tmonth=1;
-if(year==2018)
-{
-print("next year is out of boundary");
-}
-else
-tyear=year+1;
-}
-break;
-case 2:if(day<28)
-tday=day+1;
-else if(is leap(year)&&day==28)
-tday=day+1;
-else if(day==28||day==29)
-{
-tday=1;
-tmonth=3;
-}
-break;
-}
-printf("next dayis %d %d %d\n", tday,tmonth,tyear);
-return 0;
-}
+#include <stdio.h> 
+int check(int day, int month) 
+{ 
+if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) 
+{ 
+return 1; 
+} return 0; 
+} int isleap(int year) 
+{ if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+{ 
+return 1; 
+} return 0; 
+
+ } 
+int days_in_month(int month, int year) 
+{ 
+switch (month) 
+{ case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31; case 4: case 6: case 9: case 11: return 30; case 2: 
+return isleap(year) ? 29 : 28; 
+} 
+} 
+int main()
+ {
+ int day, month, year;
+ printf("Enter the date in the form of DD-MM-YYYY: "); 
+scanf("%d-%d-%d", &day, &month, &year); 
+if (year < 1) { printf("Invalid year\n"); return 1; } 
+if (month < 1 || month > 12 || check(day, month) || day < 1 || day > days_in_month(month, year))
+ { 
+printf("Invalid date\n"); 
+return 1; 
+} 
+day++; 
+if (day > days_in_month(month, year)) 
+{ day = 1; month++; if (month > 12) 
+{ 
+month = 1; year++; } }
+ if (year > 2012) 
+{ printf("Out of range\n");
+ return 1; 
+} 
+printf("The next day is: %02d-%02d-%d\n", day, month, year); 
+return 0; 
+} 
